7e116fe7e2e05c083b82b57ece72a8b9eb5b70b2
Nicolás Andrés Plaza Iñiguez <niplinig@espol.edu.ec>
2022-01-18 11:21:44 -0500

    VideoGameRentalTest listo para ser calificado

--- RentalSys/test/VideoGameRentalTest.java
+++ RentalSys/test/VideoGameRentalTest.java
@@ -1,50 +1,58 @@
 
 import org.junit.Test;
 import static org.junit.Assert.*;
+import org.junit.Before;
 
 public class VideoGameRentalTest {
 
+    protected Ps3Game ps3Game;
+    protected Xbox360Game xbox360Game;
+    protected WiiGame wiiGame;
+
     public VideoGameRentalTest() {
+    }
+
+    @Before
+    public void setUp() {
+        ps3Game = new Ps3Game("Ps3Game");
+        xbox360Game = new Xbox360Game("Xbox360Game");
+        wiiGame = new WiiGame("WiiGame");
     }
 
     @Test
     public void testGetDaysRented() {
         System.out.println("getDaysRented");
-        VideoGameRental instance = null;
+        VideoGameRental instance = new VideoGameRental(ps3Game, 0, true);
         int expResult = 0;
         int result = instance.getDaysRented();
         assertEquals(expResult, result);
-        fail("The test case is a prototype.");
     }
 
     @Test
     public void testGetVideoGame() {
         System.out.println("getVideoGame");
-        VideoGameRental instance = null;
-        Object expResult = null;
+        VideoGameRental instance = new VideoGameRental(xbox360Game, 0, true);
+        Object expResult = xbox360Game;
         Object result = instance.getVideoGame();
         assertEquals(expResult, result);
-        fail("The test case is a prototype.");
     }
 
     @Test
     public void testGetCharge() {
         System.out.println("getCharge");
-        VideoGameRental instance = null;
-        double expResult = 0.0;
+        VideoGameRental instance = new VideoGameRental(wiiGame, 10, true);
+        double expResult = wiiGame.getCharge(10, true);
         double result = instance.getCharge();
         assertEquals(expResult, result, 0.0);
-        fail("The test case is a prototype.");
     }
 
     @Test
     public void testGetFrequentRenterPoints() {
         System.out.println("getFrequentRenterPoints");
-        VideoGameRental instance = null;
-        int expResult = 0;
+        VideoGameRental instance = new VideoGameRental(wiiGame, 10, true);
+        int expResult = wiiGame.getFrequentRenterPoints(10, true);
         int result = instance.getFrequentRenterPoints();
         assertEquals(expResult, result);
-        fail("The test case is a prototype.");
     }
 
 }

2a1275e01c10ea49659cbf4cc350e9a47ef98879
Nicolás Andrés Plaza Iñiguez <niplinig@espol.edu.ec>
2022-01-18 10:52:18 -0500

    Poniendo captura en el README

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Taller09-JUnit
--Nicolás Plaza  -Kevin Valle  -José Macías
+- José Macías
+
+- Kevin Valle
+
+- Nicolás Plaza
+
+### Pruebas para la clase Movie Rental
+
+![image-20220118105005467](/home/elementary/.var/app/io.typora.Typora/config/Typora/typora-user-images/image-20220118105005467.png)
--- RentalSys/test/MovieRentalTest.java
+++ RentalSys/test/MovieRentalTest.java
@@ -1,30 +1,44 @@
 
+import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class MovieRentalTest {
 
+    protected Movie theManWhoKnewTooMuch, mulan, slumdogMillionaire;
+
     public MovieRentalTest() {
+    }
+
+    @Before
+    public void setUp() {
+        theManWhoKnewTooMuch = new Movie("The Man Who Knew Too Much", Movie.REGULAR);
+        mulan = new Movie("Mulan", Movie.CHILDRENS);
+        slumdogMillionaire = new Movie("Slumdog Millionaire", Movie.NEW_RELEASE);
+    }
+
+    @Test
+    public void testGetPriceCode() {
+        System.out.println("getPriceCode");
+        assertEquals(Movie.REGULAR, theManWhoKnewTooMuch.getPriceCode());
     }
 
     @Test
     public void testGetDaysRented() {
         System.out.println("getDaysRented");
-        MovieRental instance = null;
-        int expResult = 0;
+        MovieRental instance = new MovieRental(theManWhoKnewTooMuch, 10);
+        int expResult = 10;
         int result = instance.getDaysRented();
         assertEquals(expResult, result);
-        fail("The test case is a prototype.");
     }
 
     @Test
     public void testGetMovie() {
         System.out.println("getMovie");
-        MovieRental instance = null;
-        Movie expResult = null;
+        MovieRental instance = new MovieRental(mulan, 10);
+        Movie expResult = mulan;
         Movie result = instance.getMovie();
         assertEquals(expResult, result);
-        fail("The test case is a prototype.");
     }
 
 }

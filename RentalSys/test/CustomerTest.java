2e9d4056d8768e1c72dd2c16a25e2d5b408df5c7
josamaci <84406998+josamaci@users.noreply.github.com>
2022-01-18 11:21:53 -0500

    Add files via upload

--- RentalSys/test/CustomerTest.java
+++ RentalSys/test/CustomerTest.java
@@ -10,29 +10,24 @@ public class CustomerTest {
     @Test
     public void testAddMovieRental() {
         System.out.println("addMovieRental");
-        MovieRental arg = null;
-        Customer instance = null;
-        instance.addMovieRental(arg);
-        fail("The test case is a prototype.");
+        MovieRental arg = new MovieRental(new Movie("Title", Movie.NEW_RELEASE), 1);
+        assertNotNull("Movie rental instace shouldn't be null",arg);
     }
 
     @Test
     public void testAddVideoGameRental() {
         System.out.println("addVideoGameRental");
-        VideoGameRental arg = null;
-        Customer instance = null;
-        instance.addVideoGameRental(arg);
-        fail("The test case is a prototype.");
+        VideoGameRental arg = new VideoGameRental(new WiiGame("Mi Sports"), 1, true);
+        assertNotNull("Movie rental instace shouldn't be null",arg);
     }
 
     @Test
     public void testStatement() {
         System.out.println("statement");
-        Customer instance = null;
-        String expResult = "";
-        String result = instance.statement();
+        Customer cliente = new Customer("JUAN");
+        String expResult = "Rental Record for JUAN\nAmount owed is 0.0\nYou earned 0 frequent renter points";
+        String result = cliente.statement();
         assertEquals(expResult, result);
-        fail("The test case is a prototype.");
     }
 
 }
